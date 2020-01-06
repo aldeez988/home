4a3772802f353a4310db1d863478f3bd0789b856
elamin.fadlalla <elamin.fadlalla@capgemini.com>
2020-01-06 01:17:23 +0000

    adding the search bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  const [userQuery, setUserQuery] = useState('');
+  const updateUserQuery = event => {
+    console.log('userQuery', userQuery);
+    setUserQuery(event.target.value);
+  }
+  const searchQuery = () => {
+    window.open(`https://google.com/search?q=${userQuery}`, '_blank');
+  }
+  const handleKeyPress = event => {
+    if(event.key ==='Enter'){
+    searchQuery();
+    }
+  }
   return (
     <div className="App">
-      App
+      <h1>Hello Aldeez</h1>
+      <div className="form">
+        <input value={userQuery} onChange={updateUserQuery} onKeyPress={handleKeyPress}/>
+        <button onClick={searchQuery}>Search</button>
+      </div>
     </div>
   );
 }
