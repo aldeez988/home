829c2e5e0223192b1e5e1bb487a5e2f5bfc2446f
elamin.fadlalla <elamin.fadlalla@capgemini.com>
2020-01-08 19:10:04 +0000

    showing the stories

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Joke from './Joke';
+import Stories from './Stories'
 
 function App() {
   const [userQuery, setUserQuery] = useState('');
@@ -24,6 +25,8 @@ function App() {
       </div>
       <hr/>
       <Joke/>
+      <hr/>
+      <Stories/>
     </div>
   );
 }
