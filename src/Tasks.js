be15d6849c03470b2cb8a43e100375315dd2210d
elamin.fadlalla <elamin.fadlalla@capgemini.com>
2020-02-05 18:36:03 +0000

    displaying the tasks list

--- src/Tasks.js
+++ src/Tasks.js
@@ -1,25 +1,35 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+import uuid from 'uuid/v4';
+function Tasks() {
+    const [taskText, setTaskText] = useState('');
+    const [tasks, setTasks] = useState([]);
+    const [completedTasks, setCompletedTasks] = useState([]);
+    const updateTaskText = (e) => {
+        setTaskText(e.target.value)
+    }
+    const addTask = () => {
+        setTasks([...tasks, { taskText, id: uuid() }]);
+    }
 
-function Tasks (){
-const [taskText, setTaskText] = useState('');
-const [tasks, setTasks] = useState([]);
-const [completedTasks, setCompletedTasks] = useState([]);
-const updateTaskText = (e)=>{
-setTaskText(e.target.value)
-}
-const addTask = ()=>{
-    setTasks([...tasks, taskText]);
-}
-
-console.log('Tasks', tasks)
-return (
-    <div>
-        <h3>Tasks</h3>
-        <div className='form'>
-         <input value={taskText} onChange={updateTaskText}/>
-         <button onClick={addTask}>Add Task</button>
+    console.log('Tasks', tasks)
+    return (
+        <div>
+            <h3>Tasks</h3>
+            <div className='form'>
+                <input value={taskText} onChange={updateTaskText} />
+                <button onClick={addTask}>Add Task</button>
+            </div>
+            <div className='task-list'>
+                {
+                 tasks.map(task => {
+                const {taskText, id} = task;
+                 return (
+                      <div key={id}>{taskText}</div>
+                        )
+                    })
+                }
+            </div>
         </div>
-    </div>
-)
+    )
 }
 export default Tasks;
